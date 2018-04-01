85e3fdeb5727d285f8ee7d2ade7fc99210af888a
MewX <imewx@qq.com>
2018-04-02 00:08:46 +0930

    Added required libs and added parameters

---
+++ src/main/java/org/mewx/github/collector/Constants.java
@@ -0,0 +1,9 @@
+package org.mewx.github.collector;
+
+public class Constants {
+    // use your app information
+    public static final String GITHUB_CLIENT_ID = "Iv1.27ed2f902acf887a";
+    public static final String GITHUB_CLIENT_SECRET = "d162f756007a1977a05da6fc50efa85fb15da326";
+
+
+}
---
+++ src/main/java/org/mewx/github/collector/Main.java
@@ -0,0 +1,52 @@
+package org.mewx.github.collector;
+
+import gnu.getopt.Getopt;
+import gnu.getopt.LongOpt;
+
+public class Main {
+    public static void main(String[] args) {
+        // https://api.github.com/organizations?per_page=100&client_id=Iv1.27ed2f902acf887a&client_secret=d162f756007a1977a05da6fc50efa85fb15da326
+        StringBuffer dbName = new StringBuffer();
+        LongOpt[] longOpts = new LongOpt[] {
+                new LongOpt("help", LongOpt.NO_ARGUMENT, null, 'h'),
+                new LongOpt("collect", LongOpt.OPTIONAL_ARGUMENT, dbName, 'c'),
+                new LongOpt("parse", LongOpt.OPTIONAL_ARGUMENT, dbName, 'p'),
+                new LongOpt("analyse", LongOpt.OPTIONAL_ARGUMENT, dbName, 'a')
+        };
+
+        Getopt g = new Getopt("java -jar this.jar", args, "c::p::a::h", longOpts); // e.g. ... -crepo.db
+        g.setOpterr(false);
+        int c;
+        String arg;
+        while ((c = g.getopt()) != -1) {
+            switch (c) {
+                case 'h':
+                    System.err.println("java -jar this.jar -<c|p|a>DB.NAME | <h>");
+                    break;
+                case 'c':
+                    arg = g.getOptarg();
+                    System.err.println("collecting: " + (arg == null ? "null" : arg));
+                    break;
+                case 'p':
+                    arg = g.getOptarg();
+                    System.err.println("parse: " + (arg == null ? "null" : arg));
+                    break;
+                case 'a':
+                    arg = g.getOptarg();
+                    System.err.println("analyse: " + (arg == null ? "null" : arg));
+                    break;
+
+                case ':':
+                    System.out.println("You need an argument for option " + (char)g.getOptopt());
+                    break;
+                case '?':
+                    System.out.println("The option '" + (char)g.getOptopt() + "' is not valid");
+                    break;
+                default:
+                    System.out.println("getopt() returned " + c);
+                    break;
+            }
+        }
+
+    }
+}
